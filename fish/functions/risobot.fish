513aa84f8223e1600bcf25437b1377bd3b3f5e3e
fstromokocur <slovak70@protonmail.com>
2023-05-07 19:41:58 +0200

    commit

--- fish/functions/risobot.fish
+++
@@ -1,3 +0,0 @@
-function risobot
-DISCORD_TOKEN=MTEwMTgyNDUwMDcwNzg4NTE0Nw.GwXMb7.SDehkPJUAfxj8XRNtoIc_JKyFjP1ceCBABD3zA cargo run --manifest-path=/home/stromokocur/coding/rust/risobot/Cargo.toml
-end
