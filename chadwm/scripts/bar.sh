b877c8be7c1c2f603ecf54d3f3e5e640ec896bdb
fstromokocur <slovak70@protonmail.com>
2023-05-08 12:29:23 +0200

    commit

--- chadwm/scripts/bar.sh
+++ chadwm/scripts/bar.sh
@@ -22,8 +22,12 @@ clock() {
 	printf "^c$black^ ^b$darkblue^ DATE "
 	printf "^c$black^^b$blue^ $(date '+%a, %d.%m.%Y - %H:%M')  "
 }
+home(){
+	printf "^c$black^ ^b$darkblue^ /HOME "
+	printf "^c$white^^b$grey^ $(df -h | grep /home | awk '{print $5}')%"
+}
 
 while true; do
 
-  sleep 1 && xsetroot -name "$updates $(battery) $(brightness) $(cpu) $(mem) $(wlan) $(clock)"
+  sleep 1 && xsetroot -name "$updates $(battery) $(brightness) $(home) $(cpu) $(mem) $(wlan) $(clock)"
 done
